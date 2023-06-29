ebf7ff6ca07825149812f4b2cbc20aaad2953c92
Hugo Lobo <hugorplobo@gmail.com>
2023-06-28 21:41:42 -0300

    feat: add more rooms

---
+++ Assets/Prefabs/Rooms/Room.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Room : MonoBehaviour
+{
+    public int id;
+    public Transform entryDoor;
+    public Transform outDoor;
+}
