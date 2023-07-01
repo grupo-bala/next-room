8ef0ff94e9dd72073728a7a994b974e93b91f4f2
Hugo Lobo <hugorplobo@gmail.com>
2023-06-30 22:32:44 -0300

    feat: add door animation, feedback message and audio

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -5,14 +5,45 @@ using UnityEngine;
 public class Door : LookTrigger
 {
     public Transform pivot;
+    public Animator animator;
     private bool isOpened = false;
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.E) && this.isPlayerLooking && !isOpened)
         {
-            transform.RotateAround(this.pivot.position, Vector3.up, -90);
+            this.PlayAudio();
+            this.ShowMessage("");
+            this.animator.SetBool("IsOpened", true);
             this.isOpened = true;
         }
+    }
+
+    public override void OnPlayerLook()
+    {
+        base.OnPlayerLook();
+
+        if (!isOpened)
+        {
+            this.ShowMessage("[E] Abrir");
+        }
+    }
+
+    public override void OnPlayerStopLook()
+    {
+        base.OnPlayerStopLook();
+        this.ShowMessage("");
+    }
+
+    private void ShowMessage(string message)
+    {
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        player.GetComponentInChildren<HUD>().ShowFeedbackMessage(message);
+    }
+
+    private void PlayAudio()
+    {
+        int audioIndex = Random.Range(0, 2);
+        GetComponents<AudioSource>()[audioIndex].Play();
     }
 }
